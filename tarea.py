3c972a057cb2c291fa40613c9f11f88e565c773a
Gonzalo Blanch Dominguez <gonzaloblanchdev@gmail.com>
2026-09-17 14:02:51 +0200

    Añadir campo tag y ultima_modificacion

--- tarea.py
+++ tarea.py
@@ -1,14 +1,19 @@
+from datetime import datetime
+
 class Tarea:
 
     PRIORIDADES_VALIDAS = ("urgente", "alta", "media/alta", "media", "baja")
     ESTADOS_VALIDOS = ("pendiente", "en curso", "finalizada")
 
-    def __init__(self, titulo: str, descripcion : str, prioridad : int, estado : str, dias_restantes: int):
+    def __init__(self, titulo: str, descripcion : str, prioridad : int, estado : str, dias_restantes: int, tag: str = ""):
         self.titulo = titulo
         self.descripcion = descripcion
         self.prioridad = prioridad
         self.estado = estado
         self.dias_restantes = dias_restantes
+        self.tag = tag
+
+        self._ultima_modificacion = datetime.now()
 
     @property
     def titulo(self) -> str:
@@ -19,6 +24,7 @@ class Tarea:
         if not valor.strip():
             raise ValueError("[ERROR] El título no puede estar vacío.")
         self._titulo = valor
+        self._marcar_modificada()
 
     @property
     def descripcion(self) -> str:
@@ -27,6 +33,7 @@ class Tarea:
     @descripcion.setter
     def descripcion(self, valor: str) -> None:
         self._descripcion = valor
+        self._marcar_modificada()
 
     @property
     def prioridad(self):
@@ -37,6 +44,7 @@ class Tarea:
         if valor < 1 or valor > 5:
             raise ValueError("[ERROR] La prioridad debe estár entre 1 y 5.")
         self._prioridad = valor
+        self._marcar_modificada()
 
     @property
     def estado(self) -> str:
@@ -47,6 +55,7 @@ class Tarea:
         if valor not in Tarea.ESTADOS_VALIDOS:
             raise ValueError(f"[ERROR] Estado no válido: {valor}")
         self._estado = valor
+        self._marcar_modificada()
 
     @property
     def dias_restantes(self) -> int:
@@ -57,6 +66,18 @@ class Tarea:
         if valor < 0:
             raise ValueError("[ERROR] Los días no pueden ser negativos.")
         self._dias_restantes = valor
+        self._marcar_modificada()
+
+    @property
+    def tag(self) -> str:
+        return self._tag
+
+    tag.setter
+    def tag(self, valor: str) -> None:
+        if not isinstance(valor, str):
+            raise ValueError(f"[ERROR] El tag deve ser una cadena de texto: {valor}")
+        self._tag = valor.strip()
+        self._marcar_modificada()
 
     @property
     def clasificacion_dias(self):
@@ -67,6 +88,12 @@ class Tarea:
     def clasificacion_prioridad(self):
         return Tarea.PRIORIDADES_VALIDAS[self.prioridad - 1]
 
+    @property
+    def ultima_modificacion(self):
+        return self._ultima_modificacion
+
+    def _marcar_modificada(self):
+        self._ultima_modificacion = datetime.now()
 
     @staticmethod
     def _clasificar_por_dias(dias: int) -> str:
